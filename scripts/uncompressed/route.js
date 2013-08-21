f92f580660fcfe777f3d094671d34be26b830a3e
Jensen Tonne <jensen.tonne@gmail.com>
2013-08-21 14:33:21 +0800

    Pressing back button refreshes page by default.

--- scripts/uncompressed/route.js
+++ scripts/uncompressed/route.js
@@ -1,8 +1,30 @@
+var timestamps = []; // Array of unique timestamps.
+
 $.fn.route = function(options) {
 
 	if (this.is("a")) {
-		History.pushState($.extend({state: 1}, options), this.attr("title"), this.attr("href"));
+
+	    // Creating a unique timestamp that will be associated with the state.
+	    var t = new Date().getTime(); 
+	    timestamps[t] = t;
+
+		History.pushState($.extend({timestamp: t, refresh: true}, options), this.attr("title"), this.attr("href"));
 	}
 
 	return this;
 }
+
+History.Adapter.bind(window,'statechange',function(){
+
+    var state = History.getState();
+
+    if(state.data.timestamp in timestamps) {        
+        // Deleting the unique timestamp associated with the state
+        delete timestamps[state.data.timestamp];
+    }
+    else{
+    	if (state.data.refresh) {
+    		window.location = state.url;	
+    	}
+    }
+});
