c01d851a1a8e77b81efac58821a83fa26d0a4c81
Jensen Tonne <jstonne@stackideas.com>
2014-02-12 17:27:01 +0800

    Fixed back button not refreshing when state is in the first state in navigation history.

--- scripts/uncompressed/route.js
+++ scripts/uncompressed/route.js
@@ -5,7 +5,7 @@ $.fn.route = function(options) {
 	if (this.is("a")) {
 
 	    // Creating a unique timestamp that will be associated with the state.
-	    var t = new Date().getTime(); 
+	    var t = new Date().getTime();
 	    timestamps[t] = t;
 
 		History.pushState($.extend({timestamp: t, refresh: true}, options), this.attr("title"), this.attr("href"));
@@ -18,13 +18,19 @@ History.Adapter.bind(window,'statechange',function(){
 
     var state = History.getState();
 
-    if(state.data.timestamp in timestamps) {        
+    // Fixed back button not refreshing when
+    // state is in the first state in navigation history.
+    if (state.id===History.savedStates[0].id) {
+        window.location = state.url;
+    }
+
+    if(state.data.timestamp in timestamps) {
         // Deleting the unique timestamp associated with the state
         delete timestamps[state.data.timestamp];
     }
     else{
     	if (state.data.refresh) {
-    		window.location = state.url;	
+    		window.location = state.url;
     	}
     }
 });
