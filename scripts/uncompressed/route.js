05224d8920bbb93242c905dd1257bebb3770b4d4
Mark Lee <mark@stackideas.com>
2014-02-12 13:07:03 +0800

    Updated history to support inserting Joomla site title.

--- scripts/uncompressed/route.js
+++ scripts/uncompressed/route.js
@@ -4,11 +4,23 @@ $.fn.route = function(options) {
 
 	if (this.is("a")) {
 
+        var title   = this.attr( 'title' );
+
+        if( $.joomla.appendTitle == 'before')
+        {
+            title   = $.joomla.sitename + ' - ' + title;
+        }
+
+        if( $.joomla.appendTitle == 'after' )
+        {
+            title   = $.joomla.sitename + ' - ' + title;
+        }
+
 	    // Creating a unique timestamp that will be associated with the state.
 	    var t = new Date().getTime(); 
 	    timestamps[t] = t;
 
-		History.pushState($.extend({timestamp: t, refresh: true}, options), this.attr("title"), this.attr("href"));
+		History.pushState($.extend({timestamp: t, refresh: true}, options), title , this.attr("href"));
 	}
 
 	return this;
